c18d1feee24bfd0308411e23fb21b9ff02c85387
Marius Budreika <marius.budreika09@gmail.com>
2022-10-25 13:00:21 +0300

    exercises 2.2, 2.3*

--- part2/courseinfo/src/components/Content.js
+++ part2/courseinfo/src/components/Content.js
@@ -2,12 +2,20 @@ import React from "react";
 import Part from "./Part";
 
 const Content = ({ parts }) => {
+  const total = parts.reduce((s, p) => {
+    console.log("what is happening", s, p);
+    return s + p.exercises;
+  }, 0);
+
   return (
-    <ul style={{ listStyleType: "none", padding: 0 }}>
-      {parts.map((part) => (
-        <Part key={part.id} part={part} />
-      ))}
-    </ul>
+    <>
+      <ul style={{ listStyleType: "none", padding: 0 }}>
+        {parts.map((part) => (
+          <Part key={part.id} part={part} />
+        ))}
+      </ul>
+      <p>total of {total} exercises</p>
+    </>
   );
 };
 
