5dff49ac5695fea0b365ed2bf6b5c79d2c1bbb67
Marius Budreika <marius.budreika09@gmail.com>
2022-10-27 12:06:19 +0300

    exercise 2.7

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -13,7 +13,11 @@ const App = () => {
     const nameObject = {
       name: newName,
     };
-    setPersons(persons.concat(nameObject));
+    if (persons.some((person) => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`);
+    } else {
+      setPersons(persons.concat(nameObject));
+    }
   };
 
   return (
